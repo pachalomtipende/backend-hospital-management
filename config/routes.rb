73c9901b6903876bf526979aa34becb223e1047a
mtipende <pmtipende@gmail.com>
2026-03-29 21:58:34 +0200

    log in backend working but without forget password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "/api/create", to: "patients#send_confirmation_email"
   post "/api/verify", to: "patients#verify_email"
+  post "/api/login", to: "patients#log_in"
 
 end
